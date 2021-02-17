e5a838b21d91df863512a621fef169564cb8fda6
Alex Oliveira <alex.sdo09@gmail.com>
2021-02-17 11:28:46 -0300

    Update operadores-04.php

--- aulas-php7/operadores-04.php
+++ aulas-php7/operadores-04.php
@@ -1,18 +1,28 @@
 <?php
 
-$a = 55.5;
-$b = 55;
+$a = 35;
+$b = 35;
 
-var_dump($a > $b); 
+/*var_dump($a > $b); 
 echo "<br>";
+
 var_dump($a < $b); 
 echo "<br>";
+
 var_dump($a = $b); 
-echo "<br>"; // atribuiçãp
+echo "<br>"; // atribuição
+
 var_dump($a == $b); 
 echo "<br>"; // comparação
 
 var_dump($a === $b); 
 echo "<br>"; // comparação valor e o tipo
 
+var_dump($a != $b); 
+echo "<br>"; 
+*/
+/* ------------------- */
 
+
+
+var_dump($a <=> $b);
