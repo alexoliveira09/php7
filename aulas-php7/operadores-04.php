751bc8fc4f98a9f42dbcbbdaae9ea6b5e4a46200
Alex Oliveira <alex.sdo09@gmail.com>
2021-02-17 11:25:10 -0300

    Create operadores-04.php

---
+++ aulas-php7/operadores-04.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 55.5;
+$b = 55;
+
+var_dump($a > $b); 
+echo "<br>";
+var_dump($a < $b); 
+echo "<br>";
+var_dump($a = $b); 
+echo "<br>"; // atribuiçãp
+var_dump($a == $b); 
+echo "<br>"; // comparação
+
+var_dump($a === $b); 
+echo "<br>"; // comparação valor e o tipo
+
+
