7a6320fefcba20d04ba3742275d7d41a45993cae
Alex Oliveira <alex.sdo09@gmail.com>
2021-02-17 19:20:56 -0300

    Update if-else.php

--- aulas-php7/lacos-repeticao/if-else.php
+++ aulas-php7/lacos-repeticao/if-else.php
@@ -1,2 +1,20 @@
 <?php
+$qualSuaIdade = 68;
+$idadeCrianca = 12;
+$idadeMaior = 18;
+$idadeMelhor = 65;
 
+if ($qualSuaIdade < $idadeCrianca) {
+    echo "Criança";
+} elseif ($qualSuaIdade < $idadeMaior) {
+    echo "Adolecente";
+} elseif ($qualSuaIdade < $idadeMelhor) {
+    echo "Adulto";
+} else {
+    echo "Idoso";
+}
+
+echo "<hr>";
+/** Operador ternário */
+
+echo ($qualSuaIdade < $idadeMaior) ? "Menor de idade" : "Maior de idade";
